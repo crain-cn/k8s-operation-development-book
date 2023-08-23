19910806916270ce0f8718071029d83657942476
赵禹 <12424564+craincn@user.noreply.gitee.com>
2023-08-23 14:43:20 +0800

    add: 第三章内容与第四章内容

--- chapter-4/example/main.go
+++ chapter-4/example/main.go
@@ -1,1 +1,35 @@
-package example
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	var err error
+	var config *rest.Config
+	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	flag.Parse()
+
+	// 使用 ServiceAccount 创建集群配置（InCluster模式）
+	if config, err = rest.InClusterConfig(); err != nil {
+		// 使用 KubeConfig 文件创建集群配置
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig);
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	// 创建 clientset
+	_, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+}
