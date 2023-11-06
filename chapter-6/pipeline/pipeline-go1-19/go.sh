71d8c2d94cb9e9b5718539d52c1ab578013852e9
赵禹 <12424564+craincn@user.noreply.gitee.com>
2023-11-06 20:00:23 +0800

    add: 第6章

---
+++ chapter-6/pipeline/pipeline-go1-19/go.sh
@@ -0,0 +1,27 @@
+
+cat > main.go <<'EOF'
+package main
+
+import (
+  "log"
+  "net/http"
+  "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello World"))
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  log.Print("helloworld: listening on port 8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+EOF
+go build -o app main.go
